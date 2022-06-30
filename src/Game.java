1a267e734dee5ad64bddf5bd480485548021ee6e
abductedRhino <avocadorable@mailbox.org>
2022-06-30 12:30:04 +0200

    initial Lab11 commit

--- src/Game.java
+++ src/Game.java
@@ -1,19 +1,3 @@
-/**
- *  This class is the main class of the "World of Zuul" application. 
- *  "World of Zuul" is a very simple, text based adventure game.  Users 
- *  can walk around some scenery. That's all. It should really be extended 
- *  to make it more interesting!
- * 
- *  To play this game, create an instance of this class and call the "play"
- *  method.
- * 
- *  This main class creates and initialises all the others: it creates all
- *  rooms, creates the parser and starts the game.  It also evaluates and
- *  executes the commands that the parser returns.
- * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2016.02.29
- */
 /*
  * nextRoom = currentRoom.eastExit -> nextRoom = currentRoom.getExit("east") s.295
  * 
@@ -23,7 +7,23 @@
  * methoden printWelcome und goRoom sollen darauf zugreifen
  */
 
-public class Game 
+/**
+ * This class is the main class of the "World of Zuul" application.
+ * "World of Zuul" is a very simple, text based adventure game.  Users
+ * can walk around some scenery. That's all. It should really be extended
+ * to make it more interesting!
+ * <p>
+ * To play this game, create an instance of this class and call the "play"
+ * method.
+ * <p>
+ * This main class creates and initialises all the others: it creates all
+ * rooms, creates the parser and starts the game.  It also evaluates and
+ * executes the commands that the parser returns.
+ *
+ * @author Michael Kölling and David J. Barnes
+ * @version 2016.02.29
+ */
+public class Game
 {
     private Parser parser;
     private Room currentRoom;
@@ -31,7 +31,7 @@ public class Game
     /**
      * Create the game and initialise its internal map.
      */
-    public Game() 
+    public Game()
     {
         createRooms();
         parser = new Parser();
@@ -65,9 +65,9 @@ public class Game
     }
 
     /**
-     *  Main play routine.  Loops until end of play.
+     * Main play routine.  Loops until end of play.
      */
-    public void play() 
+    public void play()
     {            
         printWelcome();
 
